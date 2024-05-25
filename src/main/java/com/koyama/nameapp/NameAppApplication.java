21078072dcf2a5c7cbcb69259a4f2957aff45975
yoshi-koyama <koyama@reytech.co.jp>
2024-05-25 13:53:57 +0900

    プロジェクトをセットアップ

---
+++ src/main/java/com/koyama/nameapp/NameAppApplication.java
@@ -0,0 +1,13 @@
+package com.koyama.nameapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameAppApplication.class, args);
+	}
+
+}
